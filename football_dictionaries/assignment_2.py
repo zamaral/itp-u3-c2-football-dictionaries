2844edbba5f64d22f4cb58933b37b284b7718815
tcomandatore <tcomandatore@gmail.com>
2019-06-20 17:43:07 +0000

    Finished football dictionaries project

--- football_dictionaries/assignment_2.py
+++ football_dictionaries/assignment_2.py
@@ -1,2 +1,27 @@
+# from assignment_1.py import players_as_dictionaries
+
+def players_as_dictionaries(squads_list):
+    results = []
+    for players in squads_list:
+        player = {'number': players[0],
+            'position': players[1],
+            'name': players[2],
+            'date_of_birth': players[3],
+            'caps': players[4],
+            'club': players[5],
+            'country': players[6],
+            'club_country': players[7],
+            'year': players[8],}
+        results.append(player)
+    return results
+
+
 def players_by_position(squads_list):
-    pass
+    players_dict = players_as_dictionaries(squads_list)
+    results = {}
+    
+    for player in players_dict:
+        position = player['position']
+        results.setdefault(position,[])
+        results[position].append(player)
+    return results
